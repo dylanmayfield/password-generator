f07391f81e57b48de5e7d644889bb34a24bb63fc
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-08-24 16:26:46 -0500

    updated script.js

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 // Assignment code here
 
+// variables for password prompt
+var userLength = prompt ("Select a number of characters between 8 and 128");
+
+// prompt for user to select password length 
+if (userLength < 8 || userLength > 128) {
+  alert("Your character number does not meet criteria. Please select a number between 8 and 128");
+} else {
+  // variables for confirmation boxes
+  var userUpper = confirm ("Do you wish to use upper case letters in your password? Press OK for Yes,Cancel for No.");
+  var userLower = confirm ("Do you wish to use lower case letters in your password? Press OK for Yes,Cancel for No.");
+  var userNumbers = confirm ("Do you wish to use numbers in your password? Press OK for Yes,Cancel for No.");
+  var userCharacters = confirm ("Do you wish to use special characters in your password? Press OK for Yes,Cancel for No.");
+}
+
+// variables for password characters
+var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789"
+var specialCharacters = "!#$&*():;{}<>,./"
+var random = "";
+
+
+
+
+
+
+
+
+
+
+
 
 // Get references to the #generate element
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
