71a6a0eb8b3b74db50fb67e5cda72d286a281a59
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-08-24 19:33:06 -0500

    updated script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
 function generatePassword() {
   console.log("button clicked")
 
+// variables for password characters
 var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
@@ -18,11 +19,11 @@ var specialCharacters = "!#$&*():;{}<>,./"
 var password = "";
 var possibleCharacters = "";
 
-
+// user length variable and propmt message 
 var userLength = prompt ("Select a number of characters between 8 and 128");
 
 
-// 2. validate the input
+// verifying parameters meet specifications of criteria that will be input by user 
 if (userLength < 8 || userLength > 128) {
   alert("Your character number does not meet criteria. Please select a number between 8 and 128");
 
@@ -32,12 +33,12 @@ var userLower = confirm ("Confirm OK,to use lower case letters in your password.
 var userNumbers = confirm ("Confirm OK,to use numbers in your password.");
 var userCharacters = confirm ("Confirm OK,to use special characters in your password.");
 
+// 2. validate the input
   if (userUpper) {
   possibleCharacters += upperCaseLetters;
-  console.log(possibleCharacters);
+  console.log(possibleCharacters)
 } if (userLower) {
   possibleCharacters += lowerCaseLetters;
-  console.log(lowerCaseLetters)
 } if (userNumbers) {
   possibleCharacters += numbers;
 } if (userCharacters) {
@@ -46,13 +47,14 @@ var userCharacters = confirm ("Confirm OK,to use special characters in your pass
 }
 console.log(possibleCharacters)
 
-// variables for password characters
+
 
 // 3. generate password based on criteria
 for (let i = 0; i < userLength; i++) {
   password += possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)]
 }
 
+// Error message in password box if length parameters are not met 
 if (userLength < 8 || userLength > 128) 
 return ("Length does not meet criteria.")
 
